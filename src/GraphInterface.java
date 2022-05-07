2446a65f26ed7bea742e49b978ba19762ccb50d7
Melvin <melvingit@gmail.com>
2022-05-07 14:20:34 -0700

    wrote up the graph interface

--- src/GraphInterface.java
+++ src/GraphInterface.java
@@ -1,3 +1,17 @@
+//A graph interface that contains all the methods listed in the notes as well as the two travesal methods
 public interface GraphInterface<T> {
-    
+    //boolean methods
+    public boolean isEdge(int source, int target); //test whether an edge exists
+    //Methods
+    public void addEdge(int source, int target); //add an edge
+    public T getLabel(int vertex); //Accessor method to get the label of a vertex of this graph
+    public int[] neighbors(int vertex); // Obtain a list of neighbors of a specified vertex of this graph
+    public void removeEdge(int source, int target); //Remove an edge
+    public void setLabel(int vertex, T newLabel); //Change the label of a vertex of this graph
+    public int size(); //Accessor method to determine the number of vertices in this graph
+
+    //traversal methods
+    public QueueInterface<T> getBreadthFirstTraversal(T origin); //returns a queue with the breadth first traversal
+    public StackInterface<T> getDepthFirstTraversal(T origin); //returns a stack with the depth first traversal
+
 }
