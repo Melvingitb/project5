b946e95a1a6317e55ef8e37f20740452bc9e6b6b
Melvin <melvingit@gmail.com>
2022-05-08 16:45:46 -0700

    set up graph class

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,47 @@
-public class Graph {
+public class Graph<T> implements GraphInterface<T> {
+    //fields
+    private boolean[][] edges; // edges[i][j] is true if there is a vertex from i to j
+    private T[] labels; //labels [i] contains the label for vertex i
+
+    //boolean methods
+    public boolean isEdge(int source, int target){
+        return edges[source][target];
+    } //test whether an edge exists
+    //Methods
+
+    //add an edge
+    public void addEdge(int source, int target){
+
+    } 
+    //Accessor method to get the label of a vertex of this graph
+    public T getLabel(int vertex){
+
+    }
+    //Obtain a list of neighbors of a specified vertex of this graph
+    public int[] neighbors(int vertex){
+
+    }
+    //Remove an edge
+    public void removeEdge(int source, int target){
+
+    } 
+    //Change the label of a vertex of this graph
+    public void setLabel(int vertex, T newLabel){
+
+    } 
+    //Accessor method to determine the number of vertices in this graph
+    public int size(){
+
+    }
     
+    //traversal methods
+
+    //returns a queue with the breadth first traversal
+    public QueueInterface<T> getBreadthFirstTraversal(T origin){
+
+    }
+    //returns a stack with the depth first traversal
+    public StackInterface<T> getDepthFirstTraversal(T origin){
+
+    }
 }
